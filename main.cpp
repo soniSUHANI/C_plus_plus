b771652ec4a75e31d98869a672c0f1242288c430
soniSUHANI <soni03suhani@gmail.com>
2023-06-14 21:31:39 +0530

    1st c plus plus program

---
+++ main.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    //cout<<"Hello World"<<endl;
+    //cout<<"Hello soni";
+//    int a,b,c;
+//    short _sa=9;
+//    cout<<_sa;
+
+//  camalCase Notation
+    int marksInMaths=83;
+    cout<<"The marks of the student in maths is: "<<marksInMaths;
+    return 0;
+}
