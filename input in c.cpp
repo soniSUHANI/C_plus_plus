32e992a3a5a4bb3e182fe48cc0b31dad911751cd
soniSUHANI <soni03suhani@gmail.com>
2023-06-14 21:46:07 +0530

    Taking input and using operators in c programming

---
+++ input in c.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"Enter first number"<<endl;
+	cin>>a;
+	
+	cout<<"Enter second number"<<endl;
+	cin>>b;
+	
+	cout<<"The sum a+b is: "<<a+b<<endl; 
+	cout<<"The mult a*b is: "<<a*b<<endl;
+	cout<<"The diff a-b is: "<<a-b<<endl;
+	cout<<"The mod a%b is: "<<a%b<<endl;
+	cout<<"The div a/b is: "<<a/b<<endl;
+}
