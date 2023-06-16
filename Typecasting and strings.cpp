7dc772b8cce34e3d6a12eeb19227ce1fe47bbde2
soniSUHANI <soni03suhani@gmail.com>
2023-06-16 11:23:45 +0530

    strings and typecasting in c++

---
+++ Typecasting and strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+//	int a =343;
+//	float b =87.54;
+//	cout<<(float)a/34<<endl;
+//	cout<<(int)b;
+   
+   string name = "harry";
+   cout<<"The name is "<<name;
+   cout<<"The length of name is"<<name.length()<<endl;
+   cout<<"The name is "<<name.substr(1,1)<<endl;
+   cout<<"The name is "<<name.substr(2,3);
+}
