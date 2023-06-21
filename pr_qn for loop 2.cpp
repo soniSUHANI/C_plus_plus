69b641dd998e1605c2de7eb9f3f821e4dd280e8c
soniSUHANI <soni03suhani@gmail.com>
2023-06-21 13:39:03 +0530

    While and do while loop practice

---
+++ pr_qn for loop 2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//int main(){
+//	int n;
+//	cin>>n;
+//	
+//	while(n>0){
+//		cout<<n<<endl;
+//		cin>>n;
+//	}
+//	return 0;
+//}
+
+int main(){
+	int n;
+	cin>>n;
+	
+	do{
+		cout<<n<<endl;
+		cin>>n;
+	}
+	while(n>0);
+	return 0;
+}
