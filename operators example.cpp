abf479a0a5fedec04e69b478e73776ae4cc4fd4e
soniSUHANI <soni03suhani@gmail.com>
2023-06-24 13:05:36 +0530

    different types of operators

---
+++ operators example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//int main(){
+//	int i =1;
+//	
+//	i= i++ + ++i;
+//	
+//	cout<<i<<endl;
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int i=1;
+//	int j=2;
+//	int k;
+//	
+//	k = i +j + i++ + j++ + ++i + ++j;
+//	cout<<i<<" "<<j<<" "<<k<<endl;
+//	return 0;
+
+int main(){
+	int i = 0;
+	i = i++ - --i + ++i - i--;
+	cout<<i<<endl;
+	return 0;
+}
