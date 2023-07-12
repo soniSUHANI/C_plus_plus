0ecea75221f77e045ad75c59570d179ce03d5138
soniSUHANI <soni03suhani@gmail.com>
2023-07-12 21:29:54 +0530

    arrays initialisation and basics fron apna collage

---
+++ arrays.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+//int main(){
+//	int array[4] = {10,20,30,40};
+//	
+//	
+//	cout<<array[3]<<endl;
+//	return 0;
+//}
+
+int main(){
+	int n;
+	cin>>n;
+	
+	int array[n];
+	
+    for(int i=0; i<=n; i++){
+    	cin>>array[i];
+    	cout<<array[i]<<" ";
+	}
+	
+
+	
+	
+	
+	return 0;
+}
