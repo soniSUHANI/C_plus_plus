de729c4f97f696541ba7cd489aa78035d0e85f1f
soniSUHANI <soni03suhani@gmail.com>
2023-07-19 12:42:54 +0530

    record breaking day findout using logic in arrays asked in google kick start

---
+++ area of triangle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+//int main(){
+//	float b;
+//	cout<<"Enter the base: ";
+//	cin>>b;
+//	
+//	float h;
+//	cout<<"Enter the height: ";
+//	cin>>h;
+//	
+//	float area;
+//	area = (b*h)/2;
+//	cout<<"The area of triangle is: "<<area;
+//	return 0;
+//}
+
+
+//int main(){
+//	float b,h,area;
+//	cout<<"Enter the base of triangle: ";
+//	cin>>b;
+//	cout<<"Enter the height of triangle: ";
+//	cin>>h;
+//	
+//	area = (b*h)/2;
+//	cout<<"The area of triangle is: "<<area;
+//	return 0;
+//}
+
+
+int main(){
+	float b,h,area;
+	cout<<"Enter the base of triangle: ";
+	cout<<"Enter the height of triangle: ";
+	cin>>b>>h;
+	area = (b*h)/2;
+	
+	cout<<"The area of triangle is: "<<area;
+	
+	return 0;
+}
