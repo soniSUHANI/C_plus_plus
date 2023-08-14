bbf49b21ca23411b517b080eae7343af7df6cff7
soni <114800693+soniSUHANI@users.noreply.github.com>
2023-08-14 17:19:44 +0530

    Add files via upload
    insertion sort algorithm

--- insertion sort in arrays.cpp
+++ insertion sort in arrays.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
-	int n;
-	cin>>n;
-	
-	int arr[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	
+	int n=6;
+	int arr[6]={5,4,10,1,6,2};
+	int temp;
 	for(int i=1;i<n;i++){
-		int current = arr[i];
+		temp=arr[i];
 		int j =i-1;
-		while(arr[j]>current && j>=0){
+		while(arr[j]>temp && j>=0){
 			arr[j+1]=arr[j];
-			j--;
+			j=j-1;
 		}
-		arr[j+1]=current;
+		arr[j+1]=temp;
+
 	}
-	
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
-	cout<<endl;
 	return 0;
 }
-// 2 4 3 5 6
-//current = 3
-//j=1 arr[j]=arr[1] =4
-//4>3
-//
-
